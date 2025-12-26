052056111b2fbb2de916c364570c737358a84fac
Shubhpvvt <giridevansh484@gmail.com>
2025-12-26 12:41:32 +0530

    force same layout on all devices using fixed width

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -3,153 +3,142 @@ import Logo from '../part/Logo.jsx'
 
 const Login = () => {
   return (
-    <div className='w-screen  h-screen flex-col flex'>
+    <div className="w-full min-h-screen flex flex-col bg-white">
 
-      <div className='w-screen px-30 flex-1 py-20 gap-14 phone:flex-col flex justify-center items-center  phone:px-10 phone:h-fit  bg-whitew'>
+      {/* MAIN CONTENT */}
+      <div className="
+        flex-1 w-full
+        flex justify-center items-center
+        px-6 py-10
+        md:px-16 md:py-16
+      ">
 
+        <div className="
+          w-full max-w-6xl
+          flex flex-col lg:flex-row
+          gap-10
+          items-center
+        ">
 
+          {/* LEFT CARD */}
+          <div className="
+            w-full lg:w-1/3
+            px-6 py-6
+            border border-gray-200
+            rounded-xl
+            flex flex-col items-center
+            bg-white
+          ">
+            <Logo />
+            <h2 className="text-lg py-2 font-semibold">
+              Welcome Back!
+            </h2>
 
+            <div className="flex flex-col w-full gap-3">
 
-        <div className='w-1/3 phone:w-full  px-7 py-6 border border-gray-200 rounded-xl  flex flex-col justify-start items-center bg-whitew h-full '>
-          <Logo />
-          <h2 className='text-lg py-1.5 font-semibold '>Welcome Back!</h2>
+              <div className="flex flex-col gap-1">
+                <label className="text-xs">Email</label>
+                <input
+                  className="outline-none px-3 h-10 text-xs border rounded border-gray-300"
+                  placeholder="your.email@example.com"
+                  type="text"
+                />
+              </div>
 
-          <div className='flex flex-col w-full gap-2'>
+              <div className="flex flex-col gap-1">
+                <label className="text-xs">Password</label>
+                <input
+                  className="outline-none px-3 h-10 text-xs border rounded border-gray-300"
+                  placeholder="Enter your password"
+                  type="password"
+                />
+              </div>
 
-            <div className='flex h-[3.7rem]  w-full  flex-col gap-2 ' >
-              <label className='text-xs' htmlFor="usernama">Emai</label>
-              <input className='outline-none px-2 w-full h-full  text-xs border-2 rounded-sm border-gray-300 ' placeholder='your.email@example.com' type="text" id='username' name='usernama' />
+              <div className="text-xs text-[#FF321F] text-right">
+                Forgot password?
+              </div>
+
+              <button className="w-full text-sm h-9 text-white rounded bg-[#FF321F]">
+                Sign In
+              </button>
             </div>
 
-            <div className='flex h-[3.7rem]  w-full  flex-col gap-2 ' >
-              <label className='text-xs' htmlFor="usernama">Password</label>
-              <input className='outline-none px-2 w-full h-full  text-xs border-2 rounded-sm border-gray-300 ' placeholder='your.email@example.com' type="text" id='username' name='usernama' />
+            <div className="flex w-full py-3 items-center gap-2 text-xs">
+              <hr className="flex-1" /> OR <hr className="flex-1" />
             </div>
 
+            <div className="flex flex-col w-full gap-2">
+              <div className="w-full h-9 flex border rounded border-gray-200 justify-center items-center gap-2">
+                <img src="img/logo/Sign-Up/git.svg" className="w-4" />
+                <span className="text-xs">Continue with LinkedIn</span>
+              </div>
 
-            <div className='text-xs text-[#FF321F] text-right w-full'>
-              Forgot password?
+              <div className="w-full h-9 flex border rounded border-gray-200 justify-center items-center gap-2">
+                <img src="img/logo/Sign-Up/Chrome.svg" className="w-4" />
+                <span className="text-xs">Continue with Google</span>
+              </div>
             </div>
 
-
-            <button className='w-full text-sm h-8 text-white rounded-sm bg-[#FF321F]'>
-              Sign In
-            </button>
+            <div className="text-xs pt-5">
+              Don&apos;t have an account?{" "}
+              <span className="text-[#FF321F]">Sign Up</span>
+            </div>
           </div>
 
-          <div className='flex w-full py-2 justify-between items-center gap-3'><hr className=' w-full' />OR<hr className=' w-full' /></div>
-
-          <div className='flex flex-col w-full gap-2'>
-
-            <div className='w-full h-8 flex  border rounded-sm border-gray-200 justify-center items-center'>
-              <div className='w-6 h-4'><img src="img/logo/Sign-Up/git.svg" alt="" /></div>
-              <li className=' list-none  text-xs ' >Continue with LinkedIn</li>
+          {/* RIGHT IMAGE CARD */}
+          <div className="
+            w-full lg:w-1/2
+            rounded-2xl
+            bg-[#FFF1F0]
+            flex flex-col
+            justify-center items-center
+            px-6 py-10
+          ">
+            <div className="w-full max-w-md h-64">
+              <img
+                src="img/login.png"
+                className="w-full h-full object-cover rounded-xl"
+                alt=""
+              />
             </div>
 
-            <div className='w-full h-8 flex  border rounded-sm border-gray-200 justify-center items-center'>
-              <div className='w-6 h-4'><img src="img/logo/Sign-Up/Chrome.svg" alt="" /></div>
-              <li className=' list-none  text-xs ' >Continue with LinkedIn</li>
+            <div className="text-center mt-4">
+              <h3 className="font-bold text-lg">
+                Connect. Grow. Succeed.
+              </h3>
+              <h4 className="text-xs max-w-xs mx-auto">
+                Your gateway to the CTRM/ETRM professional community.
+              </h4>
             </div>
-
           </div>
-
-
-
-          <div className='text-xs pt-5 '>Don't have an account? <span className='text-[#FF321F]'>Sign Up</span></div>
-
-
-
-
-
-
-
 
         </div>
-
-
-
-
-
-        <div className='w-1/2  phone:w-full  phone:py-7  rounded-2xl bg-[#FFF1F0] flex flex-col justify-center items-center  h-full'>
-
-
-
-          <div className='w-full px-10 object-contain    h-80  phone:h-72'>
-            <img src="img/login.png" className='object-cover' alt="" />
-          </div>
-
-
-          <div className='text-center  '>
-            <h3 className='font-bold py-3 text-lx'>Connect. Grow. Succeed.</h3>
-            <h4 className='text-xs w-72'>Your gateway to the CTRM/ETRM professional community.</h4>
-          </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
       </div>
 
-
-      <div className='w-full h-10 flex phone:px-7 phone:h-32 phone:py-6 px-32 justify-between  items-center  '>
-
-        <div className=" flex gap-4 text-sm justify-center items-center ">
+      {/* FOOTER */}
+      <div className="
+        w-full
+        flex flex-col md:flex-row
+        justify-between items-center
+        gap-4
+        px-6 md:px-20
+        py-4
+        text-sm
+      ">
+        <div className="flex gap-4">
           <h4>About</h4>
           <h4>Resources</h4>
           <h4>Legal</h4>
         </div>
-        <div className=" flex gap-10 ">
 
-
-          <div className="">
-            <img src="img/logo/socal/linkedin.svg" alt="" />
-          </div>
-
-
-          <div className="">
-            <img src="img/logo/socal/tuter.svg" alt="" />
-          </div>
-
-
-          <div className="">
-            <img src="img/logo/socal/Vector.svg" alt="" />
-          </div>
-
-
-
-
+        <div className="flex gap-6">
+          <img src="img/logo/socal/linkedin.svg" />
+          <img src="img/logo/socal/tuter.svg" />
+          <img src="img/logo/socal/Vector.svg" />
         </div>
-
-
       </div>
 
-
-
-
     </div>
-
   )
 }
 
--- src/page/fixedlayout.jsx
+++ src/page/fixedlayout.jsx
@@ -1,7 +1,7 @@
 const FixedLayout = ({ children }) => {
   return (
     <div className="w-screen overflow-x-auto">
-      <div className="min-w-[1280px] scale-[0.8] origin-top-left">
+      <div className="min-w-[1200px]">
         {children}
       </div>
     </div>
